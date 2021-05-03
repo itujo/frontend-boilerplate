cafc432766ae60ea03c1eafa2fc21c3160cbe425
Ronaldo Junior <junhiowr@hotmail.com>
2021-05-03 12:22:13 -0300

    boilerplate done ?

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Center, Heading } from '@chakra-ui/react';
+import Head from 'next/head';
+
+export const Index = (): JSX.Element => (
+  <>
+    <Head>
+      <title key="pageTitle">index</title>
+    </Head>
+    <Center h="100vh">
+      <Heading>Index page</Heading>
+    </Center>
+  </>
+);
+
+export default Index;
